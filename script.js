bc06807b41a310ffd6a6e81289bbfc631e0b2bd3
OliverRob06 <oliver.stewart.robinson@gmail.com>
2025-04-17 19:39:25 +0000

    Updated Reception - Add cards for different reviews

---
+++ script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.slide-content', {
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 20,
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
