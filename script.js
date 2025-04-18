f4e07b4f682bc7d0b064b3c63bcd209a8a6fe802
OliverRob06 <oliver.stewart.robinson@gmail.com>
2025-04-18 03:34:14 +0000

    Updated Reception - Added Scores

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const swiper = new Swiper('.slide-content', {
-    loop: true,
-    slidesPerView: 4,
-    spaceBetween: 20,
-  });
-  
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 20,
+});
+
+
+
