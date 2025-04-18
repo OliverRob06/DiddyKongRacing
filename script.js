25ac4f0b214d2d8fac109103db43be658e84e088
OliverRob06 <oliver.stewart.robinson@gmail.com>
2025-04-18 00:02:42 +0000

    Updated Reception - Added animation to text descriptions

--- script.js
+++ script.js
@@ -2,10 +2,5 @@ const swiper = new Swiper('.slide-content', {
     loop: true,
     slidesPerView: 4,
     spaceBetween: 20,
-  
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
   });
   
